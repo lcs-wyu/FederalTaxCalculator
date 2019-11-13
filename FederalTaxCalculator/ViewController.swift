add0361eb77c1ee254af58d692fce4565d173d09
Wellesley Yu <byu@lcs.on.ca>
2019-11-13 17:10:26 -0500

    Near-complete version

--- FederalTaxCalculator/ViewController.swift
+++ FederalTaxCalculator/ViewController.swift
@@ -69,11 +69,13 @@ class ViewController: UIViewController {
         default:
             taxOwing = (grossAnnualIncomeAsDouble - 210371) * 0.33 + 47630 * 0.15 + 47629 * 0.205 + 52408 * 0.26 + 62704 * 0.29
         }
-        let effectiveTaxRate = Double(taxOwing/grossAnnualIncomeAsDouble * 100)
+        var effectiveTaxRate = Double()
+        effectiveTaxRate = taxOwing/grossAnnualIncomeAsDouble
+        let effectiveTaxRateInPercentage = effectiveTaxRate * 100
         // 4. Send results to the labels
         
         outputTaxesOwing .text="\(userNameAsString) your federal tax owing is $\(taxOwing)."
-        outPutEffectiveTaxRate .text="Your effective tax rate is \(effectiveTaxRate)%"
+        outPutEffectiveTaxRate .text="Your effective tax rate is \(effectiveTaxRateInPercentage)%"
     }
     
     
